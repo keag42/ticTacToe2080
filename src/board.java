3e84b3fa9ff9d240a99e7d93dd9563746506708f
keag42 <keagancam@gmail.com>
2026-03-17 11:36:33 -0400

    added space to make clearer lol

--- src/board.java
+++ src/board.java
@@ -14,7 +14,7 @@ public class board {
     public void printBoard(){
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board.length; j++){
-                System.out.print(board[i][j]);
+                System.out.print(board[i][j]  +" ");
             }
             System.out.println();
         }
