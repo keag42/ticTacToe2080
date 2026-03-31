cf71e1888679a8fad69ac7d03520ce5c2af7bf8d
keag42 <keagancam@gmail.com>
2026-03-31 16:25:11 -0400

    Cleaned up main

--- src/driver.java
+++ src/driver.java
@@ -1,23 +1,4 @@
-import java.util.Scanner;
-
-
-void main() throws IOException {
-    //Scanner input = new Scanner(System.in);
-//    board a = new board();
-//
-//    //how to use board
-//    System.out.println("before");
-//    a.printBoard();
-//
-//    a.addMove(0, 0, 'x');
-//    a.addMove(1, 0, 'x');
-//    a.addMove(2, 0, 'x');
-//
-//    System.out.println("\nAfter");
-//    a.printBoard();
-
-
-//    how to use game interface
+void main()  {
     game thisGame = new game();
     thisGame.start();
 }
